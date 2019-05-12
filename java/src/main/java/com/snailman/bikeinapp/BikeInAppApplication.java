789e584751cb654a0de9d8d7790a82b0efa79409
Dustin Saunders <dustin.saunders@sofiac.us>
2019-05-12 06:48:30 -0400

    Initial commit.

---
+++ java/src/main/java/com/snailman/bikeinapp/BikeInAppApplication.java
@@ -0,0 +1,13 @@
+package com.snailman.bikeinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeInAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BikeInAppApplication.class, args);
+  }
+}
+
---
+++ js/README.md
@@ -0,0 +1,3 @@
+# Bike-In-App
+
+The application to hold my current biking commute status. Did I bike in today? Check out the App!
