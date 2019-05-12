0753dfbca90d2a816374ab13f2ecf38d585beebe
Dustin Saunders <dustin.saunders@sofiac.us>
2019-05-12 06:57:34 -0400

    Added status controller to store state in memory.

---
+++ java/src/main/java/com/snailman/bikeinapp/controller/StatusController.java
@@ -0,0 +1,25 @@
+package com.snailman.bikeinapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/status")
+public class StatusController {
+
+  private boolean status;
+
+  @GetMapping
+  public ResponseEntity<Boolean> getStatus() {
+    return ResponseEntity.ok(this.status);
+  }
+
+  @PutMapping
+  public ResponseEntity<Boolean> updateStatus() {
+    this.status = !this.status;
+    return ResponseEntity.ok(this.status);
+  }
+}
